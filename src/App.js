a02d55cd59634ad7ade0e4c061ada31b3ed470a8
PranavJadhav011 <83655583+PranavJadhav011@users.noreply.github.com>
2023-12-22 17:28:16 +0530

    Update App.js
    removed header and post from ine 3 and 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './Header';
-import Post from "./Post";
 import Layout from './Layout';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
